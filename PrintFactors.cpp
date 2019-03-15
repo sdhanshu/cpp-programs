7504820a367fdfe60b92b135700e787e035c7593
ricksanc <sudhanshuchoudhary2005@gmail.com>
2019-03-15 13:28:38 +0530

    Added PrintFactors

---
+++ PrintFactors.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    cout<<"\nPrint Factors";
+    cout<<"\n*************";
+    int number;
+    cout<<"\nEnter the number : ";
+    cin>> number;
+
+    for ( int i = 1; i <= number ; i++)
+    {
+        if (number % i == 0)
+        {
+            cout<< i <<endl;
+        }
+    }
+}
