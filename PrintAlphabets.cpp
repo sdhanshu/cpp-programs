3231218950a6219fd8101b2e990e7cd7254cfe2c
ricksanc <sudhanshuchoudhary2005@gmail.com>
2019-03-15 12:50:30 +0530

    Added PrintEvenNumbers

--- PrintAlphabets.cpp
+++ PrintAlphabets.cpp
@@ -3,31 +3,33 @@ using namespace std;
 
 int main()
 {
-    cout<<"\nPrint Alphabets";
-    cout<<"\n~~~~~~~~~~~~~~~";
-    int choice;
-    cout<<"\nWhat type of Alphabets do you want?\n\n 1. Capital\n 2. Small \n Enter Your choice (1 or 2): ";
-    cin>>choice;
-
-    if(choice == 1)
-    {
-        for(char i = 'A'; i <= 'Z';i++)
+    do{
+        cout<<"\nPrint Alphabets";
+        cout<<"\n~~~~~~~~~~~~~~~";
+        int choice;
+        cout<<"\nWhat type of Alphabets do you want?\n\n 1. Capital\n 2. Small \n 3. Exit \n Enter Your choice (1, 2 or 3): ";
+        cin>>choice;
+        if(choice == 1)
         {
-            cout<<i<<endl;
+            for(char i = 'A'; i <= 'Z';i++)
+            {
+                cout<<i<<endl;
+            }
         }
-        
-    }
-    else if (choice == 2)
-    {
-        for(char i = 'a'; i <= 'z';i++)
+        else if (choice == 2)
         {
-            cout<<i<<endl;
+            for(char i = 'a'; i <= 'z';i++)
+            {
+                cout<<i<<endl;
+            }
         }
-    }
-    else 
-    {
-        cout<<"\nThis is a wrong choice\n";
-        cout<<"\nChoose only between 1 and 2\n"<<endl;
-    }
+        else 
+        {
+            cout<<"\nThis is a wrong choice\n";
+            cout<<"\nChoose only between 1, 2 and 3\n"<<endl;
+        }
+        cout<<"Do you want to try again? 1. Yes   2. No \n Enter your Choice: ";
+        cin>>choice;
+    }while(choice == 1);
     return 0;
 }
