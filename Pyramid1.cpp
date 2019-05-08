051f82a511a524b455663350859d96c75c5fde20
ricksanc <sudhanshuchoudhary2005@gmail.com>
2019-05-08 20:22:12 +0530

    Added Pyramid1

---
+++ Pyramid1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int rows, space, height, i, h;
+    cout<<"\nHow much height do you want for your Pyramid ? ";
+    cin >> height;
+    h = height;
+
+    for(rows = 1; rows <= height; rows++)
+    {
+        for(space = 1; space <= h; space++)
+        {
+            cout<<" ";
+        }
+
+        h--;
+
+        for(i = 1; i <= rows; i++)
+        {
+            cout<<"* ";
+        }
+
+        cout<<"\n";
+    }
+}
